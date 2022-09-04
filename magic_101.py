cd88bac99f6a23214ae470035a9309fe53b66534
Bemhreth <bemhretgezahegn@gmail.com>
2022-09-04 13:01:26 +0300

    substraction added

--- magic_101.py
+++ magic_101.py
@@ -7,3 +7,6 @@ def display_rules():
             4, then subtract from the largest number from smallest no\n\
             5, input the last digit of the answer\n")
 
+
+def subtract(num):
+    return 9 - num
