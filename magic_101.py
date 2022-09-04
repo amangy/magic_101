a40d8d32180a91b0348e9ecc906b575cac3b929a
Bemhreth <bemhretgezahegn@gmail.com>
2022-09-04 12:56:34 +0300

    display rules added

---
+++ magic_101.py
@@ -0,0 +1,9 @@
+def display_rules():
+    print("\t   1, take three digit number where \n\
+                -  there must not be  zero\n\
+                - number must be not repeated\n\
+            2,reverse the number\n\
+            3,find the largest number\n\
+            4, then subtract from the largest number from smallest no\n\
+            5, input the last digit of the answer\n")
+
